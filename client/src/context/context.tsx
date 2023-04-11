41427419a35360ef4378228991ba665790072520
Marcus Fernandez <marcustfernandez@gmail.com>
2023-04-11 13:28:05 +0200

    update favicon, fix typescript errors

--- client/src/context/context.tsx
+++ client/src/context/context.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { createClient } from "@supabase/supabase-js"
 import { AppUser } from "../utilities/utils"
 import { Database } from "../supabase"
-import { MainContext } from "./context.types"
+import { MainContext, SelectedShortcut } from "./context.types"
 import { ShortcutMember } from "utilities/types"
 
 export const supabase = createClient<Database>(
@@ -16,7 +16,7 @@ export const Context = createContext<MainContext>({} as any)
 
 const ContextProvider: FC<PropsWithChildren> = ({ children }) => {
   const [user, setUser] = useState<AppUser | null>(null)
-  const [selectedShortcut, setSelectedShortcut] = useState<ShortcutMember[] | null>(null)
+  const [selectedShortcut, setSelectedShortcut] = useState<SelectedShortcut>(null)
   return (
     <QueryClientProvider client={queryClient}>
       <Context.Provider value={{ user, setUser, selectedShortcut, setSelectedShortcut }}>{children}</Context.Provider>
